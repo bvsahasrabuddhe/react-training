0755e9a77f5d8348bb8a8e669e41e06692ec84d6
Bhalchandra <bvsahasrabuddhe@gmail.com>
2024-03-07 20:39:38 +0530

    5.12 Exercise -Updating States

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
 
 function App() {
-  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: "Bhalchandra",
+    },
+  });
 
-  const handleClick = () => {};
-  return (
-    <div>
-      <NavBar cartItemsCount={cartItems.length} />
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
-    </div>
-  );
+  const handleClick = () => {
+    setGame({ ...game, player: { ...game.player, name: "Bharat" } });
+  };
+  return <div></div>;
 }
 
 export default App;
