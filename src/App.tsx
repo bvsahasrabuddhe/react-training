140c554baef3a5b35414d89102ada4b2b5ceebfa
Bhalchandra <bvsahasrabuddhe@gmail.com>
2024-03-11 14:08:37 +0530

    6.11 Building Expense List

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
+  const expenses = [
+    { id: 1, description: "abc", amount: 10, category: "Utilities" },
+    { id: 2, description: "def", amount: 10, category: "Utilities" },
+    { id: 3, description: "ghi", amount: 10, category: "Utilities" },
+    { id: 4, description: "jlk", amount: 10, category: "Utilities" },
+    { id: 5, description: "mno", amount: 10, category: "Utilities" },
+  ];
+
   return (
     <div>
-      <Form />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => console.log("Delete", id)}
+      />
     </div>
   );
 }
